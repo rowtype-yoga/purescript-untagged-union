560a534062bab51e60cd64410f548f0026200da9
sigma-andex <sigma.andex@pm.me>
2021-10-28 19:38:47 +0100

    Prepare publish

--- test/Data/UntaggedToTagged/TaggedSpec.purs
+++ test/Data/UntaggedToTagged/TaggedSpec.purs
@@ -14,6 +14,7 @@ import Untagged.Union (type (|+|), asOneOf, toEither1)
 type ISU
   = Int |+| String |+| Boolean
 
+-- newtype only for spec (show and eq instance required)
 newtype NTISU
   = NTISU ISU
 instance Newtype NTISU ISU
